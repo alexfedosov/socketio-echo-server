c3c6b0d2ef95aa76fc42fdadbb05f3794da052a7
Alexander Fedosov <alexander.a.fedosov@gmail.com>
2016-05-11 15:42:31 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var e    = require('express');
+var sio  = require('socket.io');
+var http = require('http');
+
+var app = e();
+var server = http.createServer( app );
+
+var io  = sio.listen(server);
+
+io.sockets.on('connection', function(socket) {
+  socket.on('message', function(msg) {
+  	console.log('Receive message :' + message);
+    socket.emit('message', msg);
+  });
+});
+
+server.listen(3000);
+console.log('Server started at localhost:3000');
+console.log('Waiting for messages..');
